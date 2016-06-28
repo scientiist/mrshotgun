d2a31c6be4cce279eb4df639e429e414e6b35302
j0shgit <j0sh.oleary11@gmail.com>
2016-06-28 05:59:31 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mr Shotgun
+
+## A top-down arcade shooter
---
+++ main.lua
@@ -0,0 +1,36 @@
+------------------------------------------------------------------------------------------
+-- Top down zombie shooty game: "Mr Shotgun" by Joshua O'Leary (Juice, Csharp0, _j0shy) --
+------------------------------------------------------------------------------------------
+
+--[[
+	The Mr Shotgun License
+
+	Copyright (c) 2015 JuiceProjects
+
+	Any person is allowed to use, modify, and redistribute this software
+	free of charge, provided credit to the creator of Mr Shotgun is included.
+	Persons may not charge money for this software, or substantial portions
+	of this software.
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE. IN OTHER WORDS, I AM NOT LIABLE FOR JACK SHIT.
+
+]]--
+
+function love.load()
+
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
